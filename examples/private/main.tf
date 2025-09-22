76f2333226d0211521cb390f72c26dae813de1fd
Jacob Pack <jacobpack@navapbc.com>
2025-09-22 09:23:24 -0400

    initial push

---
+++ examples/private/main.tf
@@ -0,0 +1,203 @@
+terraform {
+  required_version = "~> 1.5"
+
+  backend "azurerm" {
+    use_azuread_auth = true
+    tenant_id = "ba06645f-e0cc-44b5-897f-34eb6aa59588"
+    subscription_id = "dec9c331-d773-4f77-a5a8-39e95699c4a5"
+    storage_account_name = "tfstfef5519ba84acf90f6f7"
+    container_name = "backends"
+    key = "private/main.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  subscription_id = "dec9c331-d773-4f77-a5a8-39e95699c4a5"
+  tenant_id = "ba06645f-e0cc-44b5-897f-34eb6aa59588"
+}
+
+
+## Section to provide a random Azure region for the resource group
+# This allows us to randomize the region for the resource group.
+module "regions" {
+  source  = "Azure/regions/azurerm"
+  version = "0.3.1"
+}
+
+data "azurerm_client_config" "current" {}
+
+# This allows us to randomize the region for the resource group.
+resource "random_integer" "region_index" {
+  max = length(module.regions.regions) - 1
+  min = 0
+}
+## End of section to provide a random Azure region for the resource group
+
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.3.0"
+}
+
+# This is required for resource modules
+resource "azurerm_resource_group" "this" {
+  location = "eastus"
+  name     = module.naming.resource_group.name_unique
+}
+
+# A vnet is required for the private endpoint.
+resource "azurerm_virtual_network" "this" {
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.virtual_network.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+  address_space       = ["192.168.0.0/24"]
+}
+
+resource "azurerm_subnet" "this" {
+  address_prefixes     = ["192.168.0.0/24"]
+  name                 = module.naming.subnet.name_unique
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  default_outbound_access_enabled = true
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
+
+
+resource "azurerm_log_analytics_workspace" "this" {
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.log_analytics_workspace.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+  internet_ingestion_enabled = false
+  internet_query_enabled = false
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_user_assigned_identity" "this" {
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.user_assigned_identity.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+
+resource "azurerm_key_vault" "keyvault" {
+  location                  = azurerm_resource_group.this.location
+  name                      = module.naming.key_vault.name_unique
+  resource_group_name       = azurerm_resource_group.this.name
+  sku_name                  = "standard"
+  tenant_id                 = data.azurerm_client_config.current.tenant_id
+  public_network_access_enabled = false
+  rbac_authorization_enabled = true
+}
+
+resource "azurerm_role_assignment" "current" {
+  principal_id         = data.azurerm_client_config.current.object_id
+  scope                = azurerm_key_vault.keyvault.id
+  role_definition_name = "Key Vault Administrator"
+}
+
+resource "azurerm_key_vault_secret" "secret" {
+  key_vault_id    = azurerm_key_vault.keyvault.id
+  name            = "secretname"
+  expiration_date = "2024-12-30T20:00:00Z"
+  value           = "password123"
+
+  depends_on = [azurerm_role_assignment.current]
+}
+
+
+
+module "test" {
+  source = "../../"
+
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.container_group.name_unique
+  os_type             = "Linux"
+  resource_group_name = azurerm_resource_group.this.name
+  restart_policy      = "Always"
+  containers = {
+    container1 = {
+      name   = "container1"
+      image  = "nginx:latest"
+      cpu    = "1"
+      memory = "2"
+      ports = [
+        {
+          port     = 80
+          protocol = "TCP"
+        }
+      ]
+      environment_variables = {
+        "ENVIRONMENT" = "dev"
+      }
+      secure_environment_variables = {
+        "SECENV" = "avmpoc"
+      }
+      volumes = {
+        secrets = {
+          mount_path = "/etc/secrets"
+          name       = "secret1"
+          secret = {
+            "password" = base64encode("password123")
+          }
+        },
+        nginx = {
+          mount_path = "/usr/share/nginx/html"
+          name       = "nginx"
+          secret = {
+            "indexpage" = base64encode("Hello, World!")
+          }
+        }
+      }
+    }
+  }
+  diagnostics_log_analytics = {
+    workspace_id  = azurerm_log_analytics_workspace.this.workspace_id
+    workspace_key = azurerm_log_analytics_workspace.this.primary_shared_key
+  }
+  exposed_ports = [
+    {
+      port     = 80
+      protocol = "TCP"
+    }
+  ]
+  managed_identities = {
+    system_assigned            = true
+    user_assigned_resource_ids = [azurerm_user_assigned_identity.this.id]
+  }
+  priority = "Regular"
+  role_assignments = {
+    role_assignment_1 = {
+      role_definition_id_or_name       = "Contributor"
+      principal_id                     = data.azurerm_client_config.current.object_id
+      skip_service_principal_aad_check = false
+    }
+  }
+  subnet_ids = [azurerm_subnet.this.id]
+  tags = {
+    cc = "avm"
+  }
+  zones = ["1"]
+}
---
+++ variables.tf
@@ -0,0 +1,267 @@
+variable "location" {
+  type        = string
+  description = "Azure region where the resource should be deployed."
+  nullable    = false
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the resource."
+  nullable    = false
+}
+
+variable "os_type" {
+  type        = string
+  description = "The operating system type for the container group."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group in which to create the resource."
+  nullable    = false
+}
+
+variable "restart_policy" {
+  type        = string
+  description = "The restart policy for the container group."
+}
+
+variable "containers" {
+  type = map(object({
+    image  = string
+    cpu    = number
+    memory = number
+    ports = list(object({
+      port     = number
+      protocol = string
+    }))
+    volumes = map(object({
+      mount_path           = string
+      name                 = string
+      read_only            = optional(bool, false)
+      empty_dir            = optional(bool, false)
+      secret               = optional(map(string), null)
+      storage_account_name = optional(string, null)
+      storage_account_key  = optional(string, null)
+      share_name           = optional(string, null)
+      git_repo = optional(object({
+        url       = optional(string, null)
+        directory = optional(string, null)
+        revision  = optional(string, null)
+      }))
+    }))
+    environment_variables        = optional(map(string), {})
+    secure_environment_variables = optional(map(string), {})
+    commands                     = optional(list(string), null)
+  }))
+  default     = {}
+  description = "A map of containers to run in the container group."
+}
+
+variable "diagnostics_log_analytics" {
+  type = object({
+    workspace_id  = string
+    workspace_key = string
+  })
+  default     = null
+  description = "The Log Analytics workspace configuration for diagnostics."
+}
+
+variable "dns_name_label" {
+  type        = string
+  default     = null
+  description = "The DNS name label for the container group."
+}
+
+variable "dns_name_label_reuse_policy" {
+  type        = string
+  default     = null
+  description = "The DNS name label reuse policy for the container group."
+}
+
+variable "dns_name_servers" {
+  type        = list(string)
+  default     = []
+  description = "A list of DNS name servers to use for the container group."
+}
+
+variable "enable_telemetry" {
+  type        = bool
+  default     = true
+  description = <<DESCRIPTION
+This variable controls whether or not telemetry is enabled for the module.
+For more information see <https://aka.ms/avm/telemetryinfo>.
+If it is set to false, then no telemetry will be collected.
+DESCRIPTION
+  nullable    = false
+}
+
+variable "exposed_ports" {
+  type = list(object({
+    port     = number
+    protocol = string
+  }))
+  default     = []
+  description = "A list of ports to expose on the container group."
+}
+
+variable "image_registry_credential" {
+  type = map(object({
+    user_assigned_identity_id = optional(string)
+    server                    = string
+    username                  = optional(string)
+    password                  = optional(string)
+  }))
+  default     = {}
+  description = "The credentials for the image registry."
+}
+
+variable "key_vault_key_id" {
+  type        = string
+  default     = null
+  description = "The Key Vault key ID for the container group."
+}
+
+variable "key_vault_user_assigned_identity_id" {
+  type        = string
+  default     = null
+  description = "The Key Vault user-assigned identity ID for the container group."
+}
+
+variable "liveness_probe" {
+  type = object({
+    exec = object({
+      command = list(string)
+    })
+    period_seconds        = number
+    failure_threshold     = number
+    success_threshold     = number
+    timeout_seconds       = number
+    initial_delay_seconds = number
+    http_get = object({
+      path         = string
+      port         = number
+      http_headers = map(string)
+    })
+    tcp_socket = object({
+      port = number
+    })
+  })
+  default     = null
+  description = "The liveness probe configuration for the container group."
+}
+
+variable "managed_identities" {
+  type = object({
+    system_assigned            = optional(bool, false)
+    user_assigned_resource_ids = optional(set(string), [])
+  })
+  default     = {}
+  description = <<DESCRIPTION
+  Controls the Managed Identity configuration on this resource. The following properties can be specified:
+  
+  - `system_assigned` - (Optional) Specifies if the System Assigned Managed Identity should be enabled.
+  - `user_assigned_resource_ids` - (Optional) Specifies a list of User Assigned Managed Identity resource IDs to be assigned to this resource.
+  DESCRIPTION
+  nullable    = false
+}
+
+variable "priority" {
+  type        = string
+  default     = null
+  description = "The Priority for the container group."
+}
+
+variable "private_endpoints" {
+  type = map(object({
+    name = optional(string, null)
+    role_assignments = optional(map(object({
+      role_definition_id_or_name             = string
+      principal_id                           = string
+      description                            = optional(string, null)
+      skip_service_principal_aad_check       = optional(bool, false)
+      condition                              = optional(string, null)
+      condition_version                      = optional(string, null)
+      delegated_managed_identity_resource_id = optional(string, null)
+      principal_type                         = optional(string, null)
+    })), {})
+    lock = optional(object({
+      kind = string
+      name = optional(string, null)
+    }), null)
+    tags                                    = optional(map(string), null)
+    subnet_resource_id                      = string
+    subresource_name                        = string # NOTE: `subresource_name` can be excluded if the resource does not support multiple sub resource types (e.g. storage account supports blob, queue, etc)
+    private_dns_zone_group_name             = optional(string, "default")
+    private_dns_zone_resource_ids           = optional(set(string), [])
+    application_security_group_associations = optional(map(string), {})
+    private_service_connection_name         = optional(string, null)
+    network_interface_name                  = optional(string, null)
+    location                                = optional(string, null)
+    resource_group_name                     = optional(string, null)
+    ip_configurations = optional(map(object({
+      name               = string
+      private_ip_address = string
+    })), {})
+  }))
+  default     = {}
+  description = "Private Endpoints Configuration"
+  nullable    = false
+}
+
+variable "readiness_probe" {
+  type = object({
+    exec = object({
+      command = list(string)
+    })
+    period_seconds        = number
+    failure_threshold     = number
+    success_threshold     = number
+    timeout_seconds       = number
+    initial_delay_seconds = number
+    http_get = object({
+      path         = string
+      port         = number
+      http_headers = map(string)
+    })
+    tcp_socket = object({
+      port = number
+    })
+  })
+  default     = null
+  description = "The readiness probe configuration for the container group."
+}
+
+variable "role_assignments" {
+  type = map(object({
+    role_definition_id_or_name             = string
+    principal_id                           = string
+    description                            = optional(string, null)
+    skip_service_principal_aad_check       = optional(bool, false)
+    condition                              = optional(string, null)
+    condition_version                      = optional(string, null)
+    delegated_managed_identity_resource_id = optional(string, null)
+    principal_type                         = optional(string, null)
+  }))
+  default     = {}
+  description = "The role assignments for the container group."
+  nullable    = false
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  default     = []
+  description = "The subnet IDs for the container group."
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = null
+  description = "(Optional) Tags of the resource."
+}
+
+variable "zones" {
+  type        = list(string)
+  default     = []
+  description = "A list of availability zones in which the resource should be created."
+}
