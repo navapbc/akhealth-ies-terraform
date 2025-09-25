a65e418343a89d53017846fb910ebff19febfb35
Jacob Pack <jacobpack@navapbc.com>
2025-09-25 19:15:55 -0400

    adjusted format and merged full aci def into file

--- examples/private/main.tf
+++ examples/private/main.tf
@@ -65,142 +65,6 @@ resource "azurerm_virtual_network" "defaultVirtualNetwork" {
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
-//START: ACI and ACI Dependency Creation
-resource "azurerm_subnet" "containerGroupSubnet" {
-  address_prefixes                = ["10.0.0.128/25"]
-  name                            = module.naming.subnet.name_unique
-  resource_group_name             = azurerm_resource_group.defaultResourceGroup.name
-  virtual_network_name            = azurerm_virtual_network.defaultVirtualNetwork.name
-  default_outbound_access_enabled = true
-  depends_on                      = [azurerm_virtual_network.defaultVirtualNetwork]
-
-  delegation {
-    name = "delegation"
-
-    service_delegation {
-      name    = "Microsoft.ContainerInstance/containerGroups"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
-  }
-}
-
-resource "azurerm_log_analytics_workspace" "defaultLogAnalyticsWorkspace" {
-  location                   = azurerm_resource_group.defaultResourceGroup.location
-  name                       = module.naming.log_analytics_workspace.name_unique
-  resource_group_name        = azurerm_resource_group.defaultResourceGroup.name
-  internet_ingestion_enabled = false
-  internet_query_enabled     = false
-  sku                        = "PerGB2018"
-}
-
-resource "azurerm_key_vault" "keyvault" {
-  location                      = azurerm_resource_group.defaultResourceGroup.location
-  name                          = module.naming.key_vault.name_unique
-  resource_group_name           = azurerm_resource_group.defaultResourceGroup.name
-  sku_name                      = "standard"
-  tenant_id                     = data.azurerm_client_config.current.tenant_id
-  public_network_access_enabled = true
-  rbac_authorization_enabled    = true
-  network_acls {
-    default_action = "Deny"
-    bypass         = "AzureServices"
-    ip_rules       = ["208.190.199.195"]
-  }
-}
-
-resource "azurerm_role_assignment" "current" {
-  principal_id         = data.azurerm_client_config.current.object_id
-  scope                = azurerm_key_vault.keyvault.id
-  role_definition_name = "Key Vault Administrator"
-}
-
-resource "azurerm_key_vault_secret" "secret" {
-  key_vault_id    = azurerm_key_vault.keyvault.id
-  name            = "secretname"
-  expiration_date = "2026-12-30T20:00:00Z"
-  value           = "password123"
-
-  depends_on = [azurerm_role_assignment.current]
-}
-
-module "containerGroupInstance" {
-  source     = "../../"
-  depends_on = [azurerm_resource_group.defaultResourceGroup]
-
-  location            = azurerm_resource_group.defaultResourceGroup.location
-  name                = module.naming.container_group.name_unique
-  os_type             = "Linux"
-  resource_group_name = azurerm_resource_group.defaultResourceGroup.name
-  restart_policy      = "Always"
-  containers = {
-    container1 = {
-      name   = "container1"
-      image  = "nginx:latest"
-      cpu    = "1"
-      memory = "2"
-      ports = [
-        {
-          port     = 80
-          protocol = "TCP"
-        }
-      ]
-      environment_variables = {
-        "ENVIRONMENT" = "dev"
-      }
-      secure_environment_variables = {
-        "SECENV" = "avmpoc"
-      }
-      volumes = {
-        secrets = {
-          mount_path = "/etc/secrets"
-          name       = "secret1"
-          secret = {
-            "password" = base64encode("password123")
-          }
-        },
-        nginx = {
-          mount_path = "/usr/share/nginx/html"
-          name       = "nginx"
-          secret = {
-            "indexpage" = base64encode("Hello, World!")
-          }
-        }
-      }
-    }
-  }
-  diagnostics_log_analytics = {
-    workspace_id  = azurerm_log_analytics_workspace.defaultLogAnalyticsWorkspace.workspace_id
-    workspace_key = azurerm_log_analytics_workspace.defaultLogAnalyticsWorkspace.primary_shared_key
-  }
-  exposed_ports = [
-    {
-      port     = 80
-      protocol = "TCP"
-    }
-  ]
-
-  managed_identities = {
-    system_assigned = true
-  }
-
-  priority = "Regular"
-  role_assignments = {
-    role_assignment_1 = {
-      role_definition_id_or_name       = "Contributor"
-      principal_id                     = data.azurerm_client_config.current.object_id
-      skip_service_principal_aad_check = false
-    }
-  }
-  subnet_ids = [azurerm_subnet.containerGroupSubnet.id]
-  tags       = {}
-  zones      = ["1"]
-}
-//END: ACI and ACI Dependency Creation
-//////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////
 //START: ACR and ACR Dependency Creation
 resource "azurerm_private_dns_zone" "this" {
   name                = "privatelink.azurecr.io"
@@ -246,6 +110,172 @@ resource "azurerm_role_assignment" "acrControlPlanRole" {
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
+//START: ACI and ACI Dependency Creation
+resource "azurerm_subnet" "containerGroupSubnet" {
+  address_prefixes                = ["10.0.0.128/25"]
+  name                            = module.naming.subnet.name_unique
+  resource_group_name             = azurerm_resource_group.defaultResourceGroup.name
+  virtual_network_name            = azurerm_virtual_network.defaultVirtualNetwork.name
+  default_outbound_access_enabled = true
+  depends_on                      = [azurerm_virtual_network.defaultVirtualNetwork]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
+
+resource "azurerm_log_analytics_workspace" "defaultLogAnalyticsWorkspace" {
+  location                   = azurerm_resource_group.defaultResourceGroup.location
+  name                       = module.naming.log_analytics_workspace.name_unique
+  resource_group_name        = azurerm_resource_group.defaultResourceGroup.name
+  internet_ingestion_enabled = false
+  internet_query_enabled     = false
+  sku                        = "PerGB2018"
+}
+
+resource "azurerm_key_vault" "keyvault" {
+  location                      = azurerm_resource_group.defaultResourceGroup.location
+  name                          = module.naming.key_vault.name_unique
+  resource_group_name           = azurerm_resource_group.defaultResourceGroup.name
+  sku_name                      = "standard"
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  public_network_access_enabled = true
+  rbac_authorization_enabled    = true
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+    ip_rules       = ["208.190.199.195"]
+  }
+}
+
+//this block gives permissions to the current identity (user, service, or managed identity)
+resource "azurerm_role_assignment" "current" {
+  principal_id         = data.azurerm_client_config.current.object_id
+  scope                = azurerm_key_vault.keyvault.id
+  role_definition_name = "Key Vault Administrator"
+}
+
+resource "azurerm_role_assignment" "kvAdminGroupAssignment" {
+  principal_id         = "58a9a684-f823-43b2-ab5f-9025a3bb2e06" //secgrp-akies-dev-kvadmin-001
+  principal_type       = "Group"
+  scope                = azurerm_key_vault.keyvault.id
+  role_definition_name = "Key Vault Administrator"
+}
+
+resource "azurerm_role_assignment" "kvDataAccessAdminGroupAssignment" {
+  principal_id         = "58a9a684-f823-43b2-ab5f-9025a3bb2e06" //secgrp-akies-dev-kvadmin-001
+  principal_type       = "Group"
+  scope                = azurerm_key_vault.keyvault.id
+  role_definition_name = "Key Vault Data Access Administrator"
+}
+
+resource "azurerm_key_vault_secret" "secret" {
+  key_vault_id    = azurerm_key_vault.keyvault.id
+  name            = "secretname"
+  expiration_date = "2026-12-30T20:00:00Z"
+  value           = "password123"
+
+  depends_on = [azurerm_role_assignment.current]
+}
+
+resource "azurerm_container_group" "containerGroupInstance" {
+  depends_on          = []
+  location            = "eastus"
+  name                = module.naming.container_group.name_unique
+  os_type             = "Linux"
+  resource_group_name = azurerm_resource_group.defaultResourceGroup.name
+  ip_address_type     = "Private"
+  priority            = "Regular"
+  restart_policy      = "Always"
+  subnet_ids          = ["${azurerm_subnet.containerGroupSubnet.id}"]
+  tags                = {}
+  zones               = ["1"]
+  dns_name_label_reuse_policy = "Unsecure"
+
+  container {
+
+    name   = "container1"
+    image  = "public.ecr.aws/docker/library/nginx:latest"
+    cpu    = 1
+    memory = 2
+
+    environment_variables        = { "ENVIRONMENT" = "dev" }
+    secure_environment_variables = { "SECENV" = "avmpoc" }
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+
+    volume {
+      mount_path = "/etc/secrets"
+      name       = "secret1"
+      secret = {
+        "password" = base64encode("password123")
+      }
+    }
+
+    volume {
+      mount_path = "/usr/share/nginx/html"
+      name       = "nginx"
+      secret = {
+        "indexpage" = base64encode("Hello, World!")
+      }
+    }
+  }
+
+
+
+  diagnostics {
+    log_analytics {
+      workspace_id  = azurerm_log_analytics_workspace.defaultLogAnalyticsWorkspace.workspace_id
+      workspace_key = azurerm_log_analytics_workspace.defaultLogAnalyticsWorkspace.primary_shared_key
+    }
+  }
+
+  exposed_port {
+    port     = "80"
+    protocol = "TCP"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  //to be defined
+  /*image_registry_credential {
+      server                    = image_registry_credential.value.server
+      password                  = image_registry_credential.value.password
+      user_assigned_identity_id = image_registry_credential.value.user_assigned_identity_id
+      username                  = image_registry_credential.value.username
+    }*/
+
+
+  timeouts {
+    create = "2h"
+    update = "2h"
+  }
+}
+
+resource "azurerm_role_assignment" "defaultContainerInstanceRoleAssignment" {
+  principal_id         = data.azurerm_client_config.current.object_id
+  scope                = azurerm_container_group.containerGroupInstance.id
+  role_definition_name = "Contributor"
+}
+
+
+
+//END: ACI and ACI Dependency Creation
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
 //START: PGSQL and PGSQL Dependency Creation
 # This ensures we have unique CAF compliant names for our resources.
 module "pgsqlNaming" {
@@ -269,11 +299,11 @@ resource "azurerm_subnet" "pgsqlSubnet" {
     }
   }
 }
+
 resource "azurerm_private_dns_zone" "pgsqlPrivateDNSZone" {
   name                = "private.postgres.database.azure.com"
   resource_group_name = azurerm_resource_group.defaultResourceGroup.name
 }
-
 
 resource "azurerm_postgresql_flexible_server" "pgsqlFlexibleServer" {
   depends_on          = [azurerm_subnet.pgsqlSubnet, azurerm_private_dns_zone.pgsqlPrivateDNSZone]
@@ -303,14 +333,21 @@ resource "azurerm_postgresql_flexible_server" "pgsqlFlexibleServer" {
   version = 16
 }
 
+/*resource "azurerm_role_assignment" "pgsqlOwner" {
+  scope                = azurerm_postgresql_flexible_server.pgsqlFlexibleServer.id
+  role_definition_name = "Owner"
+  principal_id         = "ecd5bf2d-f15c-44d1-8420-529d7c45e88b" //secgrp-akies-dev-dbadmin-001
+  principal_type       = "Group"
+}*/
+
 resource "azurerm_postgresql_flexible_server_active_directory_administrator" "pgsqlAdmin" {
   depends_on          = [azurerm_postgresql_flexible_server.pgsqlFlexibleServer]
   tenant_id           = "ba06645f-e0cc-44b5-897f-34eb6aa59588"
   server_name         = azurerm_postgresql_flexible_server.pgsqlFlexibleServer.name
   resource_group_name = azurerm_resource_group.defaultResourceGroup.name
-  principal_type      = "Group"                                # or User / ServicePrincipal
-  principal_name      = "secgrp-akies-dev-dbadmin-001"         # display name / UPN
-  object_id           = "ecd5bf2d-f15c-44d1-8420-529d7c45e88b" # GUID
+  principal_type      = "Group"
+  principal_name      = "secgrp-akies-dev-dbadmin-001"
+  object_id           = "ecd5bf2d-f15c-44d1-8420-529d7c45e88b" //secgrp-akies-dev-dbadmin-001
 }
 //END: PGSQL and PGSQL Dependency Creation
 //////////////////////////////////////////////////////////////////////////////////////////
